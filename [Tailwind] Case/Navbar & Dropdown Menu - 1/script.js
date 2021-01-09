0adf5a32e213afe2d846662b893368a0e24ed249
Artem <54672403+Artexxx@users.noreply.github.com>
2021-01-09 11:57:22 +0300

    fix code

--- [Tailwind] Case/Navbar & Dropdown Menu - 1/script.js
+++ [Tailwind] Case/Navbar & Dropdown Menu - 1/script.js
@@ -2,20 +2,20 @@ function toogleMenu() {
     document.getElementById("mobile-menu__open").classList.toggle("hidden");
     document.getElementById("mobile-menu__close").classList.toggle("hidden");
     document.getElementById("mobile-menu__items").classList.toggle("hidden");
-  }
-  
-  document.getElementById("mobile-menu__open").addEventListener(
-    "click",
-    () => {
-      toogleMenu();
-    },
-    false
-  );
-  document.getElementById("mobile-menu__close").addEventListener(
-    "click",
-    () => {
-      toogleMenu();
-    },
-    false
-  );
+}
+
+document.getElementById("mobile-menu__open").addEventListener(
+  "click",
+  () => {
+    toogleMenu();
+  },
+  false
+);
+document.getElementById("mobile-menu__close").addEventListener(
+  "click",
+  () => {
+    toogleMenu();
+  },
+  false
+);
   
